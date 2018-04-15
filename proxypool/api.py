656b6554e41f279a90c06a55cac881b395b7db6f
e2718 <e2.718@live.com>
2018-04-15 13:53:12 +0800

    ++

---
+++ proxypool/api.py
@@ -0,0 +1,49 @@
+from flask import Flask, g
+
+from .db import RedisClient
+import requests
+import re
+
+__all__ = ['app']
+
+app = Flask(__name__)
+
+
+def get_conn():
+    """
+    Opens a new redis connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'redis_client'):
+        g.redis_client = RedisClient()
+    return g.redis_client
+
+
+@app.route('/')
+def index():
+    return '<h2>Welcome to Proxy Pool System</h2>'
+
+
+@app.route('/get')
+def get_proxy():
+    """
+    Get a proxy
+    """
+    html = requests.get('http://ip.chinaz.com/211.147.67.150').text
+    ip_adress = re.compile("""<span class="Whwtdhalf w50-0">(.*?)</span>""")
+    re_ip_adress = ip_adress.findall(html)
+    conn = get_conn()
+    return conn.pop()+'\n'+str(re_ip_adress)
+
+
+@app.route('/count')
+def get_counts():
+    """
+    Get the count of proxies
+    """
+    conn = get_conn()
+    return str(conn.queue_len)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='proxy_id',
+    version='1.0.0',
+    description='High performance proxy pool',
+    long_description='A proxy pool project modified from WiseDoge/ProxyPool',
+    author=['Star',],
+    author_email='e2.718@live.com',
+    url='https://github.com/e271828182/proxy_id',
+    packages=[
+        'proxy-id'
+    ],
+    py_modules=['run'],
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'aiohttp',
+        'requests',
+        'flask',
+        'redis',
+        'pyquery'
+    ],
+    entry_points={
+        'console_scripts': ['proxy_pool_run=run:cli']
+    },
+    license='apache 2.0',
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
+)
